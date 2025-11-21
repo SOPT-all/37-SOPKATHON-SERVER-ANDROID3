6a64eee09daa7146ac622e597280933a178a53df
chyun <och2510@gmail.com>
2025-11-22 01:45:05 +0900

    feat: 애플리케이션 구성 파일 추가

---
+++ src/main/java/sopt/sopkathon37/global/config/AppConfig.java
@@ -0,0 +1,9 @@
+package sopt.sopkathon37.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class AppConfig {
+}
