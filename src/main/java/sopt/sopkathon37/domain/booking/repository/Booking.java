f099a99cbb8bdf3661386688e6d24347054ee3c2
chyun <och2510@gmail.com>
2025-11-23 06:31:20 +0900

    refactor: Booking 엔티티의 ServiceOffer와의 연관 관계를 다대일로 수정

--- src/main/java/sopt/sopkathon37/domain/booking/repository/Booking.java
+++ src/main/java/sopt/sopkathon37/domain/booking/repository/Booking.java
@@ -11,7 +11,6 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -36,7 +35,7 @@ public class Booking {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @OneToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "service_offer_id", foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
     private ServiceOffer serviceOffer;
 
