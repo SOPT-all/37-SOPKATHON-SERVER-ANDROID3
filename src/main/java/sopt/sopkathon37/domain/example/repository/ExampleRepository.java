0c654b9e47da83b3f257f1ed71d7e7f64d8d5925
chyun <och2510@gmail.com>
2025-11-23 04:00:03 +0900

    refactor: 예제 코드 제거

--- src/main/java/sopt/sopkathon37/domain/example/repository/ExampleRepository.java
+++
@@ -1,6 +0,0 @@
-package sopt.sopkathon37.domain.example.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ExampleRepository extends JpaRepository<Example, Long> {
-}
